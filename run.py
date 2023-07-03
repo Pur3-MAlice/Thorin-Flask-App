3720afac06228a6bc358f02fadd4f7b775a23900
Pur3-MAlice <aliceford98@live.co.uk>
2023-07-03 19:32:48 +0000

    adds flash and message plus styling

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -35,7 +36,9 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name "))
+        flash(
+            "Thanks {}, we have gotten your message!".format(
+                request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
